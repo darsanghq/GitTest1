1012c1a15d8dd192c29f51b15dbb88cc71ec7d29
dsds <darsangeevarghese@hq.microobjects.net>
2017-03-27 15:27:41 +0530

    test 2 added

---
+++ GitTest1/Test2.swift
@@ -0,0 +1,13 @@
+//
+//  Test2.swift
+//  GitTest1
+//
+//  Created by Darsan Geevarghese on 27/03/17.
+//  Copyright © 2017 Darsan Geevarghese. All rights reserved.
+//
+
+import UIKit
+
+class Test2: NSObject {
+
+}
