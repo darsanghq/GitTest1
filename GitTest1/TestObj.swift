fac8fc347d179927e1d46bfcbbe2cc95b602c458
dsds <darsangeevarghese@hq.microobjects.net>
2017-03-27 15:26:49 +0530

    test added

---
+++ GitTest1/TestObj.swift
@@ -0,0 +1,17 @@
+//
+//  TestObj.swift
+//  GitTest1
+//
+//  Created by Darsan Geevarghese on 27/03/17.
+//  Copyright © 2017 Darsan Geevarghese. All rights reserved.
+//
+
+import UIKit
+
+class TestObj: NSObject {
+
+    override init() {
+        print("LOL")
+    }
+    
+}
